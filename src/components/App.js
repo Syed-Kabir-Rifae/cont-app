4373b6af2a260fcdfa2d2d66a8109b97f0bda624
Syed-Kabir-Rifae <68635020+Syed-Kabir-Rifae@users.noreply.github.com>
2022-04-05 09:32:53 +0530

    intial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './Header';
+import ContactList from './ContactList';
+//import ContactCard from './ContactCard';
+import AddConatct from './AddContact';
+import './App.css';
+
+function App() {
+  const contacts = [
+    {'id': 1,'name':'Kabir', 'email':'Syed@email.com'},
+    {'id': 2,'name':'Syed', 'email' :'jawa42@email.com'}
+  ]
+  return (
+    <div>Hello World
+      <Header />
+      <AddConatct /><br></br>
+      <ContactList contacts={contacts} />
+      {/* <ContactCard /> */}
+    </div>
+    
+  
+  );
+}
+
+export default App;
